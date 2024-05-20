477e40ea6092521ac214ee4527c9f32da3c0c78d
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-05-21 00:08:51 +0300

    create editAuthor mutation to edit birthday

--- library-backend.js
+++ library-backend.js
@@ -117,6 +117,11 @@ const typeDefs = `
       author: String!
       genres: [String!]!
     ) : Book
+
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ) : Author
   }
 
   type Query {
@@ -162,6 +167,16 @@ const resolvers = {
       }
       books = books.concat(book)
       return book
+    },
+    editAuthor: (root, args) => {
+      console.log(args)
+      const author = authors.find(a => a.name === args.name)
+      if (!author) {
+        return null
+      }
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
+      return updatedAuthor
     }
   }
 }
