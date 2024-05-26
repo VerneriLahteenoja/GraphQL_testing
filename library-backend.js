2110902fe0ebaa11814247431aad9031e248a42b
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-05-27 01:05:01 +0300

    create and save authors to MongoDB

--- library-backend.js
+++ library-backend.js
@@ -2,97 +2,24 @@ const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
 const { v1: uuid } = require('uuid')
 
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821
-  },
-  { 
-    name: 'Joshua Kerievsky', // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  { 
-    name: 'Sandi Metz', // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-]
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
 
-/*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
- * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
- *
- * Spanish:
- * Podría tener más sentido asociar un libro con su autor almacenando la id del autor en el contexto del libro en lugar del nombre del autor
- * Sin embargo, por simplicidad, almacenaremos el nombre del autor en conexión con el libro
-*/
+const Author = require('./models/author')
+const Book = require('./models/book')
 
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
-  },  
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
-  },
-  {
-    title: 'Demons',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
-  },
-]
+require('dotenv').config()
+
+const MONGODB_URI = process.env.MONGODB_URI
+console.log('connecting to MongoDB')
+
+mongoose.connect(MONGODB_URI)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB', error)
+  })
 
 const typeDefs = `
   type Book {
@@ -117,6 +44,12 @@ const typeDefs = `
       author: String!
       genres: [String!]!
     ) : Book
+
+    addAuthor(
+      name: String!
+      born: Int
+      bookCount: Int
+    ) : Author
 
     editAuthor(
       name: String!
@@ -168,6 +101,10 @@ const resolvers = {
       books = books.concat(book)
       return book
     },
+    addAuthor: (root, args) => {
+      const author = new Author({ ...args })
+      return author.save()
+    },
     editAuthor: (root, args) => {
       console.log(args)
       const author = authors.find(a => a.name === args.name)
@@ -191,3 +128,81 @@ startStandaloneServer(server, {
 }).then(({ url }) => {
   console.log(`Server ready at ${url}`)
 })
+
+  //  let authors = [
+  //   {
+  //     name: 'Robert Martin',
+  //     id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
+  //     born: 1952,
+  //   },
+  //   {
+  //     name: 'Martin Fowler',
+  //     id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
+  //     born: 1963
+  //   },
+  //   {
+  //     name: 'Fyodor Dostoevsky',
+  //     id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
+  //     born: 1821
+  //   },
+  //   { 
+  //     name: 'Joshua Kerievsky', // birthyear not known
+  //     id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
+  //   },
+  //   { 
+  //     name: 'Sandi Metz', // birthyear not known
+  //     id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
+  //   },
+  // ]
+  
+  // let books = [
+  //   {
+  //     title: 'Clean Code',
+  //     published: 2008,
+  //     author: 'Robert Martin',
+  //     id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
+  //     genres: ['refactoring']
+  //   },
+  //   {
+  //     title: 'Agile software development',
+  //     published: 2002,
+  //     author: 'Robert Martin',
+  //     id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
+  //     genres: ['agile', 'patterns', 'design']
+  //   },
+  //   {
+  //     title: 'Refactoring, edition 2',
+  //     published: 2018,
+  //     author: 'Martin Fowler',
+  //     id: "afa5de00-344d-11e9-a414-719c6709cf3e",
+  //     genres: ['refactoring']
+  //   },
+  //   {
+  //     title: 'Refactoring to patterns',
+  //     published: 2008,
+  //     author: 'Joshua Kerievsky',
+  //     id: "afa5de01-344d-11e9-a414-719c6709cf3e",
+  //     genres: ['refactoring', 'patterns']
+  //   },  
+  //   {
+  //     title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
+  //     published: 2012,
+  //     author: 'Sandi Metz',
+  //     id: "afa5de02-344d-11e9-a414-719c6709cf3e",
+  //     genres: ['refactoring', 'design']
+  //   },
+  //   {
+  //     title: 'Crime and punishment',
+  //     published: 1866,
+  //     author: 'Fyodor Dostoevsky',
+  //     id: "afa5de03-344d-11e9-a414-719c6709cf3e",
+  //     genres: ['classic', 'crime']
+  //   },
+  //   {
+  //     title: 'Demons',
+  //     published: 1872,
+  //     author: 'Fyodor Dostoevsky',
+  //     id: "afa5de04-344d-11e9-a414-719c6709cf3e",
+  //     genres: ['classic', 'revolution']
+  //   },
+  // ]
