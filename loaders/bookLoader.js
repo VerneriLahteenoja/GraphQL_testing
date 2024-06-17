93dfaa28d667f6c1cf4fecc27ba740bec6791308
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-06-17 19:41:48 +0300

    create bookLoader DataLoader

---
+++ loaders/bookLoader.js
@@ -0,0 +1,14 @@
+const DataLoader = require('dataloader')
+const Book = require('../models/book')
+
+const batchBooks = async (bookIds) => {
+    const books = await Book.find({ _id: { $in: bookIds } }).populate('author')
+    const bookMap = books.reduce((acc, book) => {
+        acc[book._id] = book;
+    }, {});
+    return bookIds.map(id => bookMap[id])
+};
+
+const bookLoader = new DataLoader(batchBooks);
+
+module.exports = bookLoader;
