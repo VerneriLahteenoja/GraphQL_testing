612faeb8f473765cc2ddc52ba13f77aa17cf4bbc
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-06-22 20:53:40 +0300

    create bookCountLoader

---
+++ loaders/bookCountLoader.js
@@ -0,0 +1,21 @@
+const DataLoader = require('dataloader')
+const Book = require('../models/author')
+
+const batchBooks = async (authorIds) => {
+  const books = await Book.find({ author: { $in: authorIds } });
+  // Initialize bookcountmap
+  const booksMap = authorIds.reduce((acc, id) => {
+    acc[id] = 0;
+  }, {});
+
+  books.forEach(book => {
+    if (book.author in booksMap) {
+        booksMap[book.author] += 1;
+    }
+  });
+  return authorIds.map(authorId => booksMap[authorId]);
+};
+
+const bookCountLoader = new DataLoader(batchBooks)
+
+module.exports = bookCountLoader
