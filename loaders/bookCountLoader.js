c5e45daf7abda4a241352d981e1ebc1fcdc27054
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-06-25 22:43:37 +0300

    fix bookCountLoader book mapping

--- loaders/bookCountLoader.js
+++ loaders/bookCountLoader.js
@@ -2,11 +2,14 @@ const DataLoader = require('dataloader')
 const Book = require('../models/book')
 
 const batchBooks = async (authorIds) => {
+  console.log(authorIds)
   const books = await Book.find({ author: { $in: authorIds } });
   // Initialize bookcountmap
-  const booksMap = authorIds.reduce((acc, id) => {
-    acc[id] = 0;
-  }, {});
+  const booksMap = {}
+  authorIds.forEach(id => {
+    booksMap[id] = 0;
+  });
+  console.log(booksMap)
 
   books.forEach(book => {
     if (book.author in booksMap) {
